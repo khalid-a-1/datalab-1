6619e5968d942a43a4257aa6c4e295c73a693ba6
khalid-a-1 <93555230+khalid-a-1@users.noreply.github.com>
2021-11-02 19:13:46 -0400

    Add files via upload

---
+++ datalab1homework.c
@@ -0,0 +1,20 @@
+//1 Due Wednesday Jan. 29
+/*
+* bitAnd - x&y using only ~ and |
+*   Example: bitAnd(6, 5) = 4
+*   Legal ops: ~ |
+*   Max ops: 8
+*/
+int bitAnd(int x, int y) {
+  return 2;
+}
+
+/*
+* negate - return -x
+*   Example: negate(1) = -1.
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 5
+*/
+int negate(int x) {
+  return 2;
+}   
